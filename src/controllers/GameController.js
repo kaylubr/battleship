3c49afada265844a49cb1986a89e3a3a7107ad78
kaylubr <kylebreyes092005@gmail.com>
2026-03-07 13:56:32 +0800

    feat: set the state of gameStarted when startButton is clicked

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -1,26 +1,31 @@
 import Player from "../models/Player.js";
 
 class GameController {
-  #gameStarted;
-  #currentTurn;
-  #winner;
+  #gameStarted = false;
+  #currentTurn = null;
+  #winner = null;
   ROWS = 10;
   COLUMNS = 10;
 
   constructor() {
     this.player = new Player('PLAYER');
     this.computer = new Player('COMPUTER');
-    this.#gameStarted = false;
-    this.#currentTurn = null;
-    this.#winner = null;
   }
 
   run() {
     const leftBoard = document.querySelector('.board-left');
     const rightBoard = document.querySelector('.board-right');
+    const startButton = document.querySelector('#start-button');
 
     this.#renderBoard(leftBoard);
     this.#renderBoard(rightBoard);
+
+    startButton.addEventListener('click', this.#handleStartButton.bind(this));
+  }
+
+  #handleStartButton({ target }) {
+    target.style.display = 'none';
+    this.#gameStarted = true;
   }
 
   #renderBoard(board) {
